f9ad7053133ddf9daa95eb3ff5f86e2eeca2ed12
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:38 -0300

    Create FoneTipo.cs

---
+++ OdontoTech/Domain/FoneTipo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class FoneTipo
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+
+        public FoneTipo(int id, string tipo)
+        {
+            Id = id;
+            Tipo = tipo;
+        }
+
+        public FoneTipo()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Tipo}";
+        }
+
+        public FoneTipo(int id)
+        {
+            Id = id;
+        }
+    }
+}
