5859114489b54e3854571a8ce210e94258295bc0
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:30 -0300

    Create Paciente.cs

---
+++ OdontoTech/Domain/Paciente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; } 
+        public string Observacao { get; set; }
+        public Endereco Endereco { get; set; }
+
+        public Paciente(int id, string nome, string sobrenome, string rg, string cpf, DateTime dataNascimento, string observacao, Endereco endereco)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Rg = rg;
+            Cpf = cpf;
+            DataNascimento = dataNascimento;
+            Observacao = observacao;
+            Endereco = endereco;
+        }
+        public Paciente()
+        {
+           
+        }
+
+        public Paciente(int id)
+        {
+            Id = id;
+        }
+    }
+}
