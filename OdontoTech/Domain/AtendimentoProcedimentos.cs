580b75908fbff53a8d1bdadf115908b0dbbcc310
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:46:20 -0300

    Create AtendimentoProcedimentos.cs

---
+++ OdontoTech/Domain/AtendimentoProcedimentos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class AtendimentoProcedimentos
+    {
+        public AtendimentoProcedimentos(int id, Procedimento procedimento, Atendimento atendimento, int qtdProcedimento)
+        {
+            Id = id;
+            Procedimento = procedimento;
+            Atendimento = atendimento;
+            QtdProcedimento = qtdProcedimento;
+        }
+
+        public int Id { get; set; }
+        public Procedimento Procedimento { get; set; }
+        public Atendimento Atendimento { get; set; }
+        public int QtdProcedimento { get; set; }
+
+        public AtendimentoProcedimentos()
+        {
+
+        }
+
+        public AtendimentoProcedimentos(int id)
+        {
+            Id = id;
+        }
+    }
+}
