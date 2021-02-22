320fbd5ce4f40bd544fd9cd914d2a6397da9f953
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:45:55 -0300

    Create TipoEmbalagemDAL.cs

---
+++ OdontoTech/DataAccessLayer/TipoEmbalagemDAL.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using System.Data.SqlClient;
+using Domain;
+using System.Data.SqlClient;
+namespace DataAccessLayer
+{
+    public class TipoEmbalagemDAL
+    {
+
+        SqlConnection conn = new SqlConnection(DBConfig.CONNECTION_STRING);
+        SqlCommand cmd = new SqlCommand();
+
+        /// <summary>
+        /// Insere o  Tipo Embalagem no BD. Caso houver erro a função informa.
+        /// </summary>
+        /// <param name="TipoEmbalagem"></param>
+        public string Insert(TipoEmbalagem tipoEmbalagem)
+        {
+            cmd.Connection = conn;
+            cmd.CommandText = "INSERT INTO tipoembalagem(descricao) values(@descricao)";
+            cmd.Parameters.AddWithValue("@descricao", tipoEmbalagem.Descricao);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "Tipo de embalagem cadastrado com sucesso!";
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("Duplicate"))
+                {
+                    return ("Tipo Embalagem já cadastrada.");
+                }
+                else
+                {
+                    return ("Erro no Banco de dados. Contate o administrador.");
+                }
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+        /// <summary>
+        ///  Tenta deletar, caso der certo retorna (Tipo Embalagem deletado com êxito!) se não (Erro no Banco de dados. Contate o administrador.)
+        /// </summary>
+        /// <param name="TipoEmbalagem"></param>
+        /// <returns></returns>
+        public string Delete(TipoEmbalagem tipoEmbalagem)
+        {
+            if (tipoEmbalagem.Id == 0)
+            {
+                return "Tipo de embalagem informada inválida.";
+            }
+
+            cmd.Connection = conn;
+            cmd.CommandText = $"DELETE FROM tipoembalagem WHERE idTipoEmbalagem = {tipoEmbalagem.Id}";
+        
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "Tipo de embalagem deletado com êxito!";
+            }
+            catch (Exception)
+            {
+                return "Erro no Banco de dados. Contate o administrador.";
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+        }
+
+        /// <summary>
+        /// Tenta atualizar, caso der certo retorna (TipoEmbalagem atualizado com êxito!) se não (Erro no Banco de dados. Contate o administrador.)
+        /// </summary>
+        /// <param name="TipoEmbalagem"></param>
+        /// <returns></returns>
+        public string Update(TipoEmbalagem tipoEmbalagem)
+        {
+            cmd.Connection = conn;
+            cmd.CommandText = "UPDATE tipoembalagem SET descricao = @descricao WHERE idTipoEmbalagem = @idTipoEmbalagem";
+            cmd.Parameters.AddWithValue("@descricao", tipoEmbalagem.Descricao);
+            cmd.Parameters.AddWithValue("@idTipoEmbalagem", tipoEmbalagem.Id);
+
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return "Tipo de embalagem atualizado com êxito!";
+            }
+            catch (Exception)
+            {
+                return "Erro no Banco de dados.Contate o administrador.";
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+
+/// <summary>
+/// retorna uma lista com todos os tipos de embalagens.
+/// </summary>
+/// <returns></returns>
+        public List<TipoEmbalagem> GetAll()
+        {
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT * FROM tipoembalagem";
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<TipoEmbalagem> tipoEmbalagens = new List<TipoEmbalagem>();
+
+                while (reader.Read())
+                {
+                    TipoEmbalagem temp = new TipoEmbalagem();
+                    temp.Id = Convert.ToInt32(reader["idTipoEmbalagem"]);
+                    temp.Descricao = Convert.ToString(reader["descricao"]);
+
+                    tipoEmbalagens.Add(temp);
+                }
+
+                return tipoEmbalagens;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Erro no Banco de dados.Contate o administrador.");
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+        public TipoEmbalagem GetById(int idTipoEmbalagem)
+        {
+            cmd.Connection = conn;
+            cmd.CommandText = $"SELECT * FROM tipoembalagem WHERE idTipoEmbalagem = {idTipoEmbalagem}";
+
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                TipoEmbalagem tipoEmbalagem = new TipoEmbalagem();
+
+                while (reader.Read())
+                {
+                    tipoEmbalagem.Id = Convert.ToInt32(reader["idTipoEmbalagem"]);
+                    tipoEmbalagem.Descricao = Convert.ToString(reader["descricao"]);
+                }
+
+                return tipoEmbalagem;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Erro no Banco de dados.Contate o administrador.");
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+        public TipoEmbalagem GetLastRegister()
+        {
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT * FROM tipoembalagem ORDER BY idTipoEmbalagem DESC limit 1";
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                TipoEmbalagem tipoEmbalagem = new TipoEmbalagem();
+
+                while (reader.Read())
+                {
+                    tipoEmbalagem.Id = Convert.ToInt32(reader["idTipoEmbalagem"]);
+                    tipoEmbalagem.Descricao = Convert.ToString(reader["descricao"]);
+                }
+
+                return tipoEmbalagem;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Erro no Banco de dados.Contate o administrador.");
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+    }
+}
