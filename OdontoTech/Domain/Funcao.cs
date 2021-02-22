437b9f125d72c9d030c9720af191c7199327c932
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:35 -0300

    Create Funcao.cs

---
+++ OdontoTech/Domain/Funcao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Funcao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public double Comissao { get; set; }
+        public Funcao(int id, string nome, double salario, double comissao)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+            Comissao = comissao;
+        }
+
+        /// <summary>
+        /// construtor DAL
+        /// </summary>
+        public Funcao()
+        {
+
+        }
+
+        public Funcao(int id)
+        {
+            Id = id;
+        }
+    }
+}
