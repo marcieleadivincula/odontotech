386a553045c313d93474448d54dd5b4b6ec6c1c5
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:53:57 -0300

    Create Usuario.cs

---
+++ OdontoTech/Domain/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public Colaborador Colaborador { get; set; }
+
+        public Usuario(int id, string login, string senha)
+        {
+            Id = id;
+            Login = login;
+            Senha = senha;
+        }
+
+        public Usuario(int id, string login, string senha, Colaborador colaborador)
+        {
+            Id = id;
+            Login = login;
+            Senha = senha;
+            Colaborador = colaborador;
+        }
+
+        /// <summary>
+        /// Construtor DAL
+        /// </summary>
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(int id)
+        {
+            Id = id;
+        }
+    }
+}
