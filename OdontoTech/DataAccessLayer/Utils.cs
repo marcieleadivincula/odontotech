a596e09426e8d026406cec7af3a102bc2f381b3c
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:46:09 -0300

    Create Utils.cs

---
+++ OdontoTech/DataAccessLayer/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Utils
+    {
+        //public static Date LocalDateToSQLDate(LocalDate localdate)
+        //{
+        //    //var now = DateTime.Now;
+        //    //LocalDate today = new LocalDate(now.Year, now.Month, now.Day);
+
+        //    return Date.valueOf(localdate);
+        //}
+
+        //public static LocalDate SQLDateToLocalDate(Date date)
+        //{
+        //    return date.ToLocalDate();
+        //}
+    }
+}
