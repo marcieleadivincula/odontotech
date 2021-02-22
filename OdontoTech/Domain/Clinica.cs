5d7f0680b86057816c2dc0f65a8c5e159cfdefd6
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:46:48 -0300

    Create Clinica.cs

---
+++ OdontoTech/Domain/Clinica.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Clinica
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInauguracao { get; set; } //verificar qual usar para apenas DATA
+        public Endereco Endereco { get; set; }
+        public Estoque Estoque { get; set; }
+     
+        /// <summary>
+        /// construtor DAL.
+        /// </summary>
+        public Clinica()
+        {
+            
+        }
+
+        public Clinica(int id)
+        {
+            Id = id;
+        }
+
+        public Clinica(int id, string nome, DateTime dataInauguracao, Endereco endereco, Estoque estoque)
+        {
+            Id = id;
+            Nome = nome;
+            DataInauguracao = dataInauguracao;
+            Endereco = endereco;
+            Estoque = estoque;
+        }
+    }
+}
