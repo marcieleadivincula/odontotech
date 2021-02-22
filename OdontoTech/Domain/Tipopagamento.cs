220e218fecc4f82efe1087313ac2838c24354e3b
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:05 -0300

    Create Tipopagamento.cs

---
+++ OdontoTech/Domain/Tipopagamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class TipoPagamento
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public int Parcelas { get; set; }
+        public TipoPagamento(int id, string tipo, int parcelas)
+        {
+            Id = id;
+            Tipo = tipo;
+            Parcelas = parcelas;
+        }
+
+        /// <summary>
+        /// construtor DAL
+        /// </summary>
+        public TipoPagamento()
+        {
+
+        }
+        public TipoPagamento(int id)
+        {
+            Id = id;
+        }
+
+        
+    }
+}
