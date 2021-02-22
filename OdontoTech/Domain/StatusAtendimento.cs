d19bfd1d8a4a95acbc58256500312de5a44a8e58
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:07 -0300

    Create StatusAtendimento.cs

---
+++ OdontoTech/Domain/StatusAtendimento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class StatusAtendimento
+    {
+        public int Id { get; set; }
+        public string StatusDescricao { get; set; }
+
+        public StatusAtendimento(int id, string statusDescricao)
+        {
+            Id = id;
+            StatusDescricao = statusDescricao;
+        }
+
+        public StatusAtendimento(int id)
+        {
+            Id = id;
+        }
+
+        public StatusAtendimento()
+        {
+
+        }
+    }
+}
