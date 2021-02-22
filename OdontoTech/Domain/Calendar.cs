6c56a125c236164869a2d0a271d1d66b62665009
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:55:05 -0300

    Create Calendar.cs

---
+++ OdontoTech/Domain/Calendar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Calendar
+    {
+        public virtual string Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Location { get; set; }
+    }
+}
