d70e310c4ea065937bdf5b32cb67c08243748ac4
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:00 -0300

    Create Tipoprocedimento.cs

---
+++ OdontoTech/Domain/Tipoprocedimento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class TipoProcedimento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+        public TipoProcedimento(int id, string nome, double valor)
+        {
+            Id = id;
+            Nome = nome;
+            Valor = valor;
+        }
+        public TipoProcedimento()
+        {
+
+        }
+
+        public TipoProcedimento(int id)
+        {
+            Id = id;
+        }
+    }
+}
