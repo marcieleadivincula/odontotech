dd3db1e46b6620840dbac87f4a68bda9d2f3c071
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:44 -0300

    Create Estado.cs

---
+++ OdontoTech/Domain/Estado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Estado
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Pais Pais { get; set; }
+
+        public Estado(int id, string nome, Pais pais)
+        {
+            Id = id;
+            Nome = nome;
+            Pais = pais;
+        }
+        /// <summary>
+        /// construtor para DAL.
+        /// </summary>
+        public Estado()
+        {
+
+        }
+        public Estado(int id)
+        {
+            Id = id;
+        }
+    }
+}
