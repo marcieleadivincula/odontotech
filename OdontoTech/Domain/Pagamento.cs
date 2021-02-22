71764fbe62ec75d1c5d5036556efb4afe90fc75d
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:27 -0300

    Create Pagamento.cs

---
+++ OdontoTech/Domain/Pagamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Pagamento
+    {
+        public int Id { get; set; }
+        public DateTime DataPagamento { get; set; } 
+        public double ValorPagamento { get; set; } 
+        public TipoPagamento TipoPagamento { get; set; }
+        public Paciente Paciente { get; set; }
+
+        /// <summary>
+        /// construtor DAL.
+        /// </summary>
+        public Pagamento()
+        {
+
+        }
+
+        public Pagamento(int id)
+        {
+            Id = id;
+        }
+
+        public Pagamento(int id, DateTime dataPagamento, double valorPagamento, TipoPagamento tipoPagamento, Paciente paciente)
+        {
+            Id = id;
+            DataPagamento = dataPagamento;
+            ValorPagamento = valorPagamento;
+            TipoPagamento = tipoPagamento;
+            Paciente = paciente;
+        }
+    }
+}
