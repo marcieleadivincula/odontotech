767020f26c1451bcfcf9d1c4a5ae0e75d74258eb
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:43:49 -0300

    Create CodsegurancaBLL.cs

---
+++ OdontoTech/BusinessLogicalLayer/CodsegurancaBLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BusinessLogicalLayer
+{
+   public class CodsegurancaBLL
+    {
+        CodsegurancaDAL dal = new CodsegurancaDAL();
+
+        public bool EnviaEMAIL(string email)
+        {
+            return dal.EnviaEmail(email);
+        }
+        public bool VerificaCodigo(string codigo,string email)
+        {
+            if (Convert.ToString(dal.GetCODByEmail(email)) == codigo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void DeletaByEmail(string email)
+        {
+            dal.DeleteByEmail(email);
+        }
+
+    }
+}
