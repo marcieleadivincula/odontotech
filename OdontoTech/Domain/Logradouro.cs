fbec8e924e844b8c9d8f351c0426d9fbb7bd8b96
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:33 -0300

    Create Logradouro.cs

---
+++ OdontoTech/Domain/Logradouro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Logradouro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Bairro Bairro { get; set; }
+
+        public Logradouro(int id, string nome, Bairro bairro)
+        {
+            Id = id;
+            Nome = nome;
+            Bairro = bairro;
+        }
+       
+        /// <summary>
+        /// CostrutorDAL.
+        /// </summary>
+        public Logradouro()
+        {
+
+        }
+
+        public Logradouro(int id)
+        {
+            Id = id;
+        }
+    }
+}
