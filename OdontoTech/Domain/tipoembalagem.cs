cc9f513d98e763f2d9d73b5260794fdc18f9ce9f
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:15 -0300

    Create tipoembalagem.cs

---
+++ OdontoTech/Domain/tipoembalagem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class TipoEmbalagem
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public TipoEmbalagem(int id, string descricao)
+        {
+            Id= id;
+            Descricao = descricao;
+        }
+        public TipoEmbalagem()
+        {
+
+        }
+    }
+}
