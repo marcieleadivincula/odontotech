c89bb93a5105ce3273aa1269b78a32793cebe7f0
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:24 -0300

    Create Pais.cs

---
+++ OdontoTech/Domain/Pais.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Pais
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Pais(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        /// <summary>
+        /// Pais DAL
+        /// </summary>
+        public Pais()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Nome}";
+        }
+
+        public Pais(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
