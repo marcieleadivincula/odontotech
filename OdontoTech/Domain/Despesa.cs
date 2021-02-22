d48eba50cce62bde59ab87ad171e8bb0a0277bc9
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:54:50 -0300

    Create Despesa.cs

---
+++ OdontoTech/Domain/Despesa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Despesa
+    {
+        public Despesa(int idDespesa, DateTime data, double valor, string descricao)
+        {
+            this.idDespesa = idDespesa;
+            Data = data;
+            Valor = valor;
+            Descricao = descricao;
+        }
+
+        public Despesa()
+        {
+        }
+
+        public int idDespesa { get; set; }
+        public DateTime Data { get; set; }
+        public double Valor { get; set; }
+        public string Descricao { get; set; }
+    }
+}
