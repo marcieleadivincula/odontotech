1193c4fdfc5748cffc41868da9e235e394e635bf
marcieleadivincula <marcieleadivincula@gmail.com>
2021-02-22 16:46:53 -0300

    Create Contato.cs

---
+++ OdontoTech/Domain/Contato.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Fone { get; set; }
+        public FoneTipo FoneTipo { get; set; }
+        public string Email { get; set; }
+        public Paciente Paciente { get; set; }
+        public Colaborador Colaborador { get; set; }
+
+        public Contato(int id, string fone, FoneTipo foneTipo, string email, Colaborador colaborador)
+        {
+            Id = id;
+            Fone = fone;
+            FoneTipo = foneTipo;
+            Email = email;
+            Colaborador = colaborador;
+        }
+
+        public Contato(int id, string fone, FoneTipo foneTipo, string email, Paciente paciente)
+        {
+            Id = id;
+            Fone = fone;
+            FoneTipo = foneTipo;
+            Email = email;
+            Paciente = paciente;
+        }
+
+        public Contato(int id)
+        {
+            Id = id;
+        }
+
+        public Contato()
+        {
+
+        }
+    }
+}
